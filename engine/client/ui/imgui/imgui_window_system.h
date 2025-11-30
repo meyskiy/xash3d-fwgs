f6e4955271e05141efbcfbb0ffc3efefca83d783
meyskiy <alosiumfree@gmail.com>
2025-11-30 00:52:32 +0000

    fix

--- engine/client/ui/imgui/imgui_window_system.h
+++ engine/client/ui/imgui/imgui_window_system.h
@@ -1,20 +1,6 @@
 #pragma once
 #include "imgui_window.h"
-
-// Forward declaration to avoid including <vector> in header on Android
-// This prevents STL conflicts between old STL and new libc++
-#if __ANDROID__
-// Prevent old STL from being included
-#define _STL_PAIR_H
-#define _STL_UTILITY_H
-#define _STL_CONFIG_H
-namespace std {
-    template<typename T, typename Alloc = void>
-    class vector;
-}
-#else
 #include <vector>
-#endif
 
 class CImGuiWindowSystem
 {
@@ -24,7 +10,7 @@ public:
 
 private:
     CImGuiWindowSystem();
-    ~CImGuiWindowSystem();
+    ~CImGuiWindowSystem() {};
     CImGuiWindowSystem(const CImGuiWindowSystem &) = delete;
     CImGuiWindowSystem &operator=(const CImGuiWindowSystem &) = delete;
 
@@ -36,10 +22,6 @@ private:
     bool KeyInput(bool keyDown, int keyNumber, const char *bindName);
     bool CursorRequired();
 
-#if __ANDROID__
-    std::vector<IImGuiWindow*> *m_WindowList;
-#else
     std::vector<IImGuiWindow*> m_WindowList;
-#endif
 };
 
